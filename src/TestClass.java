e76c6972a21e8559dec9b82a157bc28b0620793d
jinil lee <dlwlsdlf0723@naver.com>
2021-12-17 21:40:44 +0900

    commit

--- src/TestClass.java
+++ src/TestClass.java
@@ -3,5 +3,6 @@ public class TestClass {
 	public static void main(String[] args) {
 		System.out.println("Hello! this is Github Practicing");
 		System.out.println("git허브 연습 ㅎㅎ");
+		System.out.println("수정");
 	}
 }
