5bbb735cd2195663da4208bf5b050e8f98333858
jinil lee <dlwlsdlf0723@naver.com>
2021-12-17 20:43:24 +0900

    This is Commit Massage!!

---
+++ src/Coding_test3_1.java
@@ -0,0 +1,48 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Coding_test3_1 {
+	public static void main(String[] args) {
+		
+		Scanner in = new Scanner(System.in);
+
+		int N = in.nextInt(); //N 배열의 크기
+		int M = in.nextInt(); //M 더하는 횟수
+		int K = in.nextInt(); //K 같은 인덱스가 K번초과해선 안됨
+		
+		//N 크기만큼 배열생성
+		int arr[] = new int[N];
+		
+		//N 개수만큼 입력받아서 배열에 넣기
+		for (int i = 0; i < N; i++) {
+		 arr[i]=in.nextInt();	
+		}
+		
+		Arrays.sort(arr);// Array.sort(배열이름) > 배열 오름 차순으로
+		
+		int first = arr[N-1]; 
+		int second = arr[N-2];
+		int result = 0;
+		
+		int count = 0;
+		
+		
+		while (count < M) {
+			for (int i = 0; i < K; i++) {
+				result += first;
+				count +=1;
+				if (M==count) {
+					break;
+				}
+			}
+			if (M==count) {
+				break;
+			}
+			result+=second;
+			count+=1;
+		}
+		
+		System.out.println(result);
+		
+	}
+}
